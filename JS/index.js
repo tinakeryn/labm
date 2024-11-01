8a93900cedd02958d3bb6e3546b3b72e2219ec39
tinakeryn <145064910+tinakeryn@users.noreply.github.com>
2024-11-01 17:13:08 +0100

    Multiples modifs
    Ajout de :
    
    - Nav,
    - HGPO,
    - T21

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,51 @@
+//? --> Fonctions génériques
+//* Retirer la class hidden
+function showNextElement(buttonId) {
+  const button = document.getElementById(buttonId);
+  if (button) {
+    const nextElement = button.nextElementSibling;
+    if (nextElement && nextElement.classList.contains("hidden")) {
+      nextElement.classList.remove("hidden");
+    }
+  }
+}
+//* Ajouter la class hidden
+function hideElements(elementIds) {
+  elementIds.forEach(function (id) {
+    const element = document.getElementById(id);
+    if (element) {
+      element.classList.add("hidden");
+    }
+  });
+}
+//* Remonter en haut avec la flèche
+//! Faire apparaître la flèche quand on scroll vers le bas
+window.addEventListener("scroll", function () {
+  const arrowUp = document.getElementById("arrowUp");
+  if (window.scrollY > 100) {
+    arrowUp.style.display = "block";
+  } else {
+    arrowUp.style.display = "none";
+  }
+});
+//! Scroller vers le haut au clic sur la flèche
+document.getElementById("arrowUp").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+//* Faire défiler la fenêtre vers une section spécifique
+function scrollToSection(buttonId, sectionId) {
+  const button = document.getElementById(buttonId);
+  const section = document.getElementById(sectionId);
+
+  button.addEventListener("click", function () {
+    section.scrollIntoView({ behavior: "smooth" });
+  });
+}
+//? Fonctions génériques <--
+
+//? --> Générer groupe sanguin aléatoire
 // Statistiques de répartition des groupes sanguins
-const bloodGroups = [
+const bloodTypes = [
   { type: "O+", percentage: 37 },
   { type: "A+", percentage: 27 },
   { type: "B+", percentage: 23 },
@@ -9,27 +55,202 @@ const bloodGroups = [
   { type: "B-", percentage: 1 },
   { type: "AB-", percentage: 1 },
 ];
-
 // Fonction pour générer un groupe sanguin aléatoire
-function generateBloodGroup() {
+function generateBloodType() {
   let rand = Math.random() * 100;
   let cumulativePercentage = 0;
 
-  for (let i = 0; i < bloodGroups.length; i++) {
-    cumulativePercentage += bloodGroups[i].percentage;
+  for (let i = 0; i < bloodTypes.length; i++) {
+    cumulativePercentage += bloodTypes[i].percentage;
     if (rand <= cumulativePercentage) {
-      return bloodGroups[i].type;
+      return bloodTypes[i].type;
     }
   }
 }
-
 // Événement de clic sur le bouton
-document.getElementById("generateButton").addEventListener("click", function () {
+document.getElementById("generateBloodTypeButton").addEventListener("click", function () {
   let resultElement = document.getElementById("result");
-  resultElement.textContent = generateBloodGroup();
+  resultElement.textContent = generateBloodType();
 });
+//? Générer groupe sanguin aléatoire <--
 
-// Dictionnaires des germes
+//? --> Générer un résultat HGPO aléatoire
+// Seuils pour le diagnostic de diabète gestationnel
+const hgpoLimits = {
+  fasting: 0.92, // à jeun (en g/L)
+  oneHour: 1.8, // 1 heure post-charge
+  twoHours: 1.53, // 2 heures post-charge
+};
+// Générer des valeurs HGPO aléatoires en tenant compte des seuils diagnostiques
+function generateHGPOResult() {
+  const fasting = (Math.random() * 0.3 + 0.7).toFixed(2); // Entre 0.7 et 1.0
+  const oneHour = (Math.random() * 0.4 + 1.4).toFixed(2); // Entre 1.4 et 1.8
+  const twoHours = (Math.random() * 0.3 + 1.2).toFixed(2); // Entre 1.2 et 1.5
+
+  // Affichage du diagnostic basé sur les résultats
+  let diagnosis = "Résultat normal";
+  if (
+    parseFloat(fasting) >= hgpoLimits.fasting ||
+    parseFloat(oneHour) >= hgpoLimits.oneHour ||
+    parseFloat(twoHours) >= hgpoLimits.twoHours
+  ) {
+    diagnosis = "Diabète gestationnel suspecté";
+  }
+
+  return {
+    fasting,
+    oneHour,
+    twoHours,
+    diagnosis,
+  };
+}
+// Événement de clic pour générer le résultat HGPO
+document.getElementById("generateHGPOButton").addEventListener("click", function () {
+  const hgpoResult = generateHGPOResult();
+  const resultDiv = document.getElementById("hgpoResult");
+
+  resultDiv.classList.remove("hidden"); // Affiche l'antibiogramme
+
+  resultDiv.innerHTML = `À jeun : ${hgpoResult.fasting} g/L
+    <br>1 heure : ${hgpoResult.oneHour} g/L
+    <br>2 heures : ${hgpoResult.twoHours} g/L
+    <br><strong>Diagnostic :</strong> ${hgpoResult.diagnosis}  `;
+
+  console.log(hgpoResult);
+  console.log(resultDiv);
+
+  // Rendre visible les résultats
+  showNextElement("generateHGPOButton");
+});
+//? Générer un résultat HGPO aléatoire <--
+
+//? --> Calcul du risque de T21
+//* Calculer le risque
+function calculateT21Risk(age, nuchalThickness) {
+  let ageRisk = "";
+  let nuchalRisk = "";
+
+  // Détermination du risque en fonction de l'âge
+  if (age < 25) {
+    ageRisk = "1/1500";
+  } else if (age >= 25 && age < 30) {
+    ageRisk = "1/1000";
+  } else if (age >= 30 && age < 31) {
+    ageRisk = "1/900";
+  } else if (age === 31) {
+    ageRisk = "1/800";
+  } else if (age === 32) {
+    ageRisk = "1/600";
+  } else if (age === 33) {
+    ageRisk = "1/500";
+  } else if (age === 34) {
+    ageRisk = "1/400";
+  } else if (age === 35) {
+    ageRisk = "1/270";
+  } else if (age === 36) {
+    ageRisk = "1/200";
+  } else if (age === 37) {
+    ageRisk = "1/150";
+  } else if (age === 38) {
+    ageRisk = "1/100";
+  } else if (age === 39) {
+    ageRisk = "1/70";
+  } else if (age === 40) {
+    ageRisk = "1/50";
+  } else if (age === 41) {
+    ageRisk = "1/40";
+  } else if (age === 42) {
+    ageRisk = "1/30";
+  } else {
+    ageRisk = "1/20";
+  }
+
+  // Ajustement du risque selon la clarté nucale
+  if (nuchalThickness <= 2.5) {
+    nuchalRisk = "faible";
+  } else if (nuchalThickness > 2.5 && nuchalThickness <= 3.5) {
+    nuchalRisk = "modéré";
+  } else {
+    nuchalRisk = "élevé";
+  }
+
+  // Estimation finale
+  let finalRisk;
+  if (ageRisk === "1/1500" && nuchalRisk === "faible") {
+    finalRisk = "Risque faible";
+  } else if (
+    (ageRisk === "1/1000" && nuchalRisk === "faible") ||
+    (ageRisk === "1/900" && nuchalRisk === "faible") ||
+    (ageRisk === "1/800" && nuchalRisk === "faible") ||
+    (ageRisk === "1/600" && nuchalRisk === "faible")
+  ) {
+    finalRisk = "Risque faible";
+  } else if (
+    (ageRisk === "1/500" && nuchalRisk === "faible") ||
+    (ageRisk === "1/400" && nuchalRisk === "faible") ||
+    (ageRisk === "1/270" && nuchalRisk === "faible") ||
+    (ageRisk === "1/200" && nuchalRisk === "faible") ||
+    (ageRisk === "1/150" && nuchalRisk === "modéré") ||
+    (ageRisk === "1/100" && nuchalRisk === "modéré")
+  ) {
+    finalRisk = "Risque modéré";
+  } else {
+    finalRisk = "Risque élevé";
+  }
+
+  return { ageRisk, nuchalRisk, finalRisk };
+}
+//* Faire le calcul au clic ou en appuyant sur Entrée
+document.getElementById("generateT21Button").addEventListener("click", function () {
+  const dobInput = document.getElementById("dobInput").value;
+  const nuchalInput = parseFloat(document.getElementById("nuchalInput").value);
+
+  if (!dobInput || isNaN(nuchalInput)) {
+    alert("Veuillez entrer une date de naissance valide et une clarté nucale.");
+    return;
+  }
+
+  // Calcul de l'âge de la patiente
+  const dob = new Date(dobInput);
+  const today = new Date();
+  const age = today.getFullYear() - dob.getFullYear();
+  // Ajustement si l'anniversaire n'est pas encore passé cette année
+  if (
+    today.getMonth() < dob.getMonth() ||
+    (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate())
+  ) {
+    age--;
+  }
+
+  // Calcul du risque
+  const { ageRisk, nuchalRisk, finalRisk } = calculateT21Risk(age, nuchalInput);
+
+  // Affichage du résultat
+  const t21ResultElement = document.getElementById("t21Result");
+  t21ResultElement.innerHTML = `Risque basé sur l'âge: ${ageRisk}, <br>Risque basé sur la clarté nucale: ${nuchalRisk},<br>Estimation finale: ${finalRisk}`;
+  t21ResultElement.classList.remove("hidden"); // Affiche le résultat
+
+  // Affichage du bouton pour fermer ou naviguer
+  showNextElement("generateT21Button");
+});
+// Cible les éléments input et le bouton
+const dobInput = document.getElementById("dobInput");
+const nuchalInput = document.getElementById("nuchalInput");
+const generateT21Button = document.getElementById("generateT21Button");
+// Fonction pour vérifier les conditions et simuler un clic sur le bouton
+function triggerGenerateT21(event) {
+  if (event.key === "Enter" && dobInput.value && nuchalInput.value) {
+    event.preventDefault(); // Empêche le comportement par défaut du formulaire
+    generateT21Button.click(); // Simule un clic sur le bouton
+  }
+}
+// Ajouter les écouteurs d'événement pour chaque input
+dobInput.addEventListener("keydown", triggerGenerateT21);
+nuchalInput.addEventListener("keydown", triggerGenerateT21);
+//? Calcul du risque de T21 <--
+
+//? --> Générer des germes aléatoires
+//* Dictionnaires des germes
 const urinaryGerms = [
   "Escherichia coli",
   "Klebsiella pneumoniae",
@@ -38,7 +259,6 @@ const urinaryGerms = [
   "Staphylococcus saprophyticus",
   "Enterococcus faecalis",
 ];
-
 const pulmonaryGerms = [
   "Streptococcus pneumoniae",
   "Haemophilus influenzae",
@@ -48,7 +268,6 @@ const pulmonaryGerms = [
   "Chlamydia pneumoniae",
   "Candida albicans",
 ];
-
 const orlGerms = [
   "Streptococcus pneumoniae",
   "Haemophilus influenzae",
@@ -57,8 +276,7 @@ const orlGerms = [
   "Staphylococcus aureus",
   "Candida albicans",
 ];
-
-// Dictionnaires des antibiotiques
+//* Dictionnaires des antibiotiques
 const urinaryATB = [
   "Amoxicilline + acide clavulanique",
   "Ceftriaxone",
@@ -71,7 +289,6 @@ const urinaryATB = [
   "Tétracycline",
   "Triméthroprime + sulfamides",
 ];
-
 const pulmonaryATB = [
   "Amoxicilline + acide clavulanique",
   "Azithromycine",
@@ -84,7 +301,6 @@ const pulmonaryATB = [
   "Fluconazole",
   "Itraconazole",
 ];
-
 const orlATB = [
   "Amoxicilline + acide clavulanique",
   "Azithromycine",
@@ -97,8 +313,7 @@ const orlATB = [
   "Vancomycine",
   "Fluconazole",
 ];
-
-// Dictionnaire des résistances connues
+//* Dictionnaire des résistances connues
 const resistancesConnues = {
   "Escherichia coli": ["Ciprofloxacine", "Nitrofurantoïne"],
   "Klebsiella pneumoniae": [
@@ -126,13 +341,14 @@ const resistancesConnues = {
   "Fusobacterium necrophorum": [],
   "Corynebacterium diphtheriae": [],
 };
-
+//! ECBU
+//* Générer un germe urinaire aléatoire
 // Fonction pour générer un germe aléatoire à partir du dictionnaire des germes urinaires
 function generateUrinaryGerm() {
   const randomIndex = Math.floor(Math.random() * urinaryGerms.length);
   return urinaryGerms[randomIndex];
 }
-
+//* Générer un antibiogramme urinaire
 // Fonction pour générer un antibiogramme virtuel
 function generateAntibiogram(germe) {
   const antibiogram = {};
@@ -155,7 +371,6 @@ function generateAntibiogram(germe) {
 
   return antibiogram;
 }
-
 // Fonction pour formater les résultats selon l'ordre de urinaryATB
 function formatAntibiogram(antibiogram) {
   const orderedResults = [];
@@ -164,7 +379,7 @@ function formatAntibiogram(antibiogram) {
   });
   return orderedResults.join("\n");
 }
-
+//* Fonction qui génère un ECBU au clic
 // Événement de clic sur le bouton de génération de germe pour l'ECBU
 document.getElementById("generateECBUGermButton").addEventListener("click", function () {
   const germe = generateUrinaryGerm();
@@ -177,14 +392,17 @@ document.getElementById("generateECBUGermButton").addEventListener("click", func
   const ecbuATBResultElement = document.getElementById("ecbuATBResult");
   ecbuATBResultElement.textContent = "Antibiogramme :\n\n" + formatAntibiogram(antibiogram);
   ecbuATBResultElement.classList.remove("hidden"); // Affiche l'antibiogramme
-});
 
+  showNextElement("generateECBUGermButton");
+});
+//! ECBC
+//* Générer un germe ECBC aléatoire
 // Fonction pour générer un germe aléatoire à partir du dictionnaire des germes pulmonaires
 function generatePulmonaryGerm() {
   const randomIndex = Math.floor(Math.random() * pulmonaryGerms.length);
   return pulmonaryGerms[randomIndex];
 }
-
+//* Générer un antibiogramme ECBC
 function generatePulmonaryAntibiogram(germe) {
   const antibiogram = {};
 
@@ -206,7 +424,6 @@ function generatePulmonaryAntibiogram(germe) {
 
   return antibiogram;
 }
-
 // Fonction pour formater les résultats selon l'ordre de pulmonaryATB
 function formatPulmonaryAntibiogram(antibiogram) {
   const orderedResults = [];
@@ -215,7 +432,7 @@ function formatPulmonaryAntibiogram(antibiogram) {
   });
   return orderedResults.join("\n");
 }
-
+//* Fonction qui génère un ECBC au clic
 // Événement de clic sur le bouton de génération de germe pour l'ECBC
 document.getElementById("generateECBCGermButton").addEventListener("click", function () {
   const germe = generatePulmonaryGerm();
@@ -229,15 +446,17 @@ document.getElementById("generateECBCGermButton").addEventListener("click", func
   ecbcATBResultElement.textContent =
     "Antibiogramme :\n\n" + formatPulmonaryAntibiogram(antibiogram);
   ecbcATBResultElement.classList.remove("hidden"); // Affiche l'antibiogramme
-});
 
+  showNextElement("generateECBCGermButton");
+});
+//! ORL
+//* Générer un germe ORL aléatoire
 // Fonction pour générer un germe aléatoire à partir du dictionnaire des germes ORL
 function generateOrlGerm() {
   const randomIndex = Math.floor(Math.random() * orlGerms.length);
   return orlGerms[randomIndex];
 }
-
-// Fonction pour générer un antibiogramme virtuel pour les germes ORL
+//* Générer un antibiogramme ORL
 function generateOrlAntibiogram(germe) {
   const antibiogram = {};
 
@@ -259,7 +478,6 @@ function generateOrlAntibiogram(germe) {
 
   return antibiogram;
 }
-
 // Fonction pour formater les résultats selon l'ordre de orlATB
 function formatOrlAntibiogram(antibiogram) {
   const orderedResults = [];
@@ -268,8 +486,7 @@ function formatOrlAntibiogram(antibiogram) {
   });
   return orderedResults.join("\n");
 }
-
-// Événement de clic sur le bouton de génération de germe pour le prélèvement ORL
+//* Fonction qui génère un résultat de prélèvement ORL au clic
 document.getElementById("generateORLGermButton").addEventListener("click", function () {
   const germe = generateOrlGerm();
   const antibiogram = generateOrlAntibiogram(germe);
@@ -281,4 +498,33 @@ document.getElementById("generateORLGermButton").addEventListener("click", funct
   const orlATBResultElement = document.getElementById("orlATBResult");
   orlATBResultElement.textContent = "Antibiogramme :\n\n" + formatOrlAntibiogram(antibiogram);
   orlATBResultElement.classList.remove("hidden"); // Affiche l'antibiogramme
+  showNextElement("generateORLGermButton");
 });
+//? Générer des germes aléatoires <--
+
+//? Afficher toute la section au clic sur le bouton
+scrollToSection("generateBloodTypeButton", "bloodTypeSection");
+scrollToSection("generateECBUGermButton", "ecbuSection");
+scrollToSection("generateECBCGermButton", "ecbcSection");
+scrollToSection("generateORLGermButton", "orlSection");
+scrollToSection("generateHGPOButton", "hgpoSection");
+scrollToSection("generateT21Button", "t21Section");
+//? Afficher toute la section au clic sur le bouton <--
+
+//? Masquer la section au clic sur la croix
+document.getElementById("ecbuClose").addEventListener("click", function () {
+  hideElements(["ecbuClose", "ecbuResult", "ecbuATBResult"]);
+});
+document.getElementById("ecbcClose").addEventListener("click", function () {
+  hideElements(["ecbcClose", "ecbcResult", "ecbcATBResult"]);
+});
+document.getElementById("orlClose").addEventListener("click", function () {
+  hideElements(["orlClose", "orlResult", "orlATBResult"]);
+});
+document.getElementById("hgpoClose").addEventListener("click", function () {
+  hideElements(["hgpoClose", "hgpoResult"]);
+});
+document.getElementById("t21Close").addEventListener("click", function () {
+  hideElements(["t21Close", "t21Result"]);
+});
+//? Masquer la section au clic sur la croix <--
